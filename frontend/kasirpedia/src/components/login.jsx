4bb233b66e186de472f99573a9aa9290a4bd5a98
hwangtommy <73519047+hwangtommy@users.noreply.github.com>
2023-02-03 10:07:12 +0700

    Cropped logo

--- frontend/kasirpedia/src/components/login.jsx
+++ frontend/kasirpedia/src/components/login.jsx
@@ -1,7 +1,17 @@
-
+import { VStack, Center, Image } from "@chakra-ui/react";
+import logo from "../logos/Kasirpedia-logos_white.png"
 
 function Login(){
+    return(
+        <>
+        <Center>
+            <VStack bg={"black"}>
+                <Image src={logo}/>
 
+            </VStack>
+        </Center>
+        </>
+    )
 }
 
 export default Login
