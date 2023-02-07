0bab08e8514366007ccf03d1647c86ecaf0a13ca
hwangtommy <73519047+hwangtommy@users.noreply.github.com>
2023-02-07 14:22:26 +0700

    Added features

--- frontend/kasirpedia/src/components/itemlist.jsx
+++ frontend/kasirpedia/src/components/itemlist.jsx
@@ -1,16 +1,17 @@
-import { Flex, Input, Select } from '@chakra-ui/react'
+import { Flex, Input, Select, Button, Grid, GridItem } from '@chakra-ui/react'
 import ItemCard from './itemcard'
 
 export default function ItemList(){
     return(
         <>        
-        <Flex wrap={"wrap"} maxW={"1100px"} m={"10px"} border={"1px solid grey"} borderRadius={10}>
+        <Flex wrap={"wrap"} maxW={"1100px"} m={"10px"}>
             <Input placeholder='Search items..' size='md' m={"12px"}/>
             <Select placeholder='Categories' mx={"12px"} my={"6px"}>
                 <option value='option1'>Option 1</option>
                 <option value='option2'>Option 2</option>
                 <option value='option3'>Option 3</option>
             </Select>
+            <Button size='md' m={"12px"} w="100%">Add Item</Button>
             <ItemCard name="Kopi" price="10000" photo="https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG" />
             <ItemCard name="Kopi" price="10000" photo="https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG" />
             <ItemCard name="Kopi" price="10000" photo="https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG" />
