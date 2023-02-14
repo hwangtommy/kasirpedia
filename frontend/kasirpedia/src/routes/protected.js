22c8032000de4c750f71d309e20283fe9923acbf
hwangtommy <73519047+hwangtommy@users.noreply.github.com>
2023-02-14 10:16:59 +0700

    added routes

---
+++ frontend/kasirpedia/src/routes/protected.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function ProtectedPage({
+    children,
+    needLogin = false,
+    guestOnly = false,
+    authRoles = ["User", "Admin"]
+}) {
+    let navigate = useNavigate();
+    const userSelector = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (needLogin && !userSelector?.id) {
+            return navigate("/", { replace: true });
+        }
+
+        if (guestOnly && userSelector.id) {
+            return navigate("/", { replace: true });
+        }
+
+        if (authRoles === "Admin"){
+            return navigate ("/admin", { replace: true })
+        }
+    }, []);
+    return children;
+}
